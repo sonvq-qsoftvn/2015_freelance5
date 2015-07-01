afc3a7445d06f0691268fb0e309efd056716bc35
Ngo Quoc Thai <TNQ.e250@gmail.com>
2015-07-01 09:01:41 +0700

    update

--- js/script_home.js
+++ js/script_home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 jQuery(function ($) {
     $(function () {
         $('.carousel').carouFredSel({
@@ -29,35 +28,3 @@ jQuery(function ($) {
 
     });
 });
-=======
-jQuery(function ($) {
-    $(function () {
-        $('.carousel').carouFredSel({
-            width: $(".slider-container").width(),
-            height: $(".slider-container").height(),
-            align: false,
-            items: {
-                visible: 1,
-                width: 'variable',
-                height: 'variable'
-            },
-            scroll: {
-                fx: "crossfade"
-            },
-            prev: '.prev-slider',
-            next: '.next-slider'
-        });
-
-        $(window).resize(function () {
-            var newCss = {
-                width: $('.slider-container').width(),
-                height: $('.slider-container').height()
-            };
-            $('.carousel').css('width', newCss.width * 4);
-            $('.carousel').parent().css(newCss);
-            $('.carousel div').css(newCss);
-        }).resize();
-
-    });
-});
->>>>>>> 725663292a3b44b95c1911a9d12bdf130c0c8d0b
--- js/script_lastminute.js
+++ js/script_lastminute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 jQuery(function ($) {
     $(function () {
         $("#slider-range").slider({
@@ -30,36 +29,3 @@ jQuery(function ($) {
                 " - " + tmpFirstEnd  + ":00");
     });
 });
-=======
-jQuery(function ($) {
-    $(function () {
-        $("#slider-range").slider({
-            range: true,
-            min: 6,
-            max: 23,
-            values: [6, 23],
-            slide: function (event, ui) {
-                var tmpstart = ui.values[ 0 ];
-                var tmpend = ui.values[ 1 ];
-                if(tmpstart < 10) {
-                    tmpstart = "0" + tmpstart;
-                }
-                if(tmpend < 10) {
-                    tmpend = "0" + tmpend;
-                }
-                $("#time-range").val(tmpstart + ":00" + " - " + tmpend + ":00");
-            }
-        });
-        var tmpFirstStart = $("#slider-range").slider("values", 0);
-        var tmpFirstEnd = $("#slider-range").slider("values", 1);
-        if(tmpFirstStart < 10) {
-            tmpFirstStart = "0" + tmpFirstStart;
-        }
-        if(tmpFirstEnd < 10) {
-            tmpFirstEnd = "0" + tmpFirstEnd;
-        }
-        $("#time-range").val(tmpFirstStart + ":00" +
-                " - " + tmpFirstEnd  + ":00");
-    });
-});
->>>>>>> 725663292a3b44b95c1911a9d12bdf130c0c8d0b
