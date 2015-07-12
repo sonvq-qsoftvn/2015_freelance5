9e1780c290cb823858443cfb43c1b63f95d1be1d
Dean <thainq@qsoftvietnam.com>
2015-07-12 16:54:41 +0700

    update booking tour, update animation,

--- js/booking.js
+++ js/booking.js
@@ -1,5 +1,24 @@
 $(function () {
-    $("#booking-step-by-tep").on('click', 'li a', function(){
-//        alert('fas');
+	$("#choose-date").click(function(e){
+		e.preventDefault();
+		$("#date-booking").removeClass('hidden');
+	});
+
+    $("#step-1, #step-2, .hour-picker").on('click', 'li', function(){
+    	$("#step-1 ul li, #step-2 ul li, .hour-picker li").removeClass("active");
+    	$(this).addClass("active");
     });
+
+     $('#datetimepicker12').datetimepicker({
+                inline: true,
+                sideBySide: false,
+                format:'d.m.Y',                
+                locale: 'fr',
+                viewMode: 'days',
+                daysOfWeekDisabled: [0, 6],
+                allowInputToggle: false
+            }).on('dp.change', function(){
+     			$(".hour-picker").removeClass('hidden');
+   			});;
+
 });
