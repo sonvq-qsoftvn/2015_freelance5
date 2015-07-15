934dd536e0e935bdd6a779106b92bc7fffbd8a9b
SonVQ <sonvq@qsoft.com.vn>
2015-07-15 18:13:26 +0700

    commit uielement

--- js/booking.js
+++ js/booking.js
@@ -1,24 +1,29 @@
 $(function () {
-	$("#choose-date").click(function(e){
-		e.preventDefault();
-		$("#date-booking").removeClass('hidden');
-	});
-
-    $("#step-1, #step-2, .hour-picker").on('click', 'li', function(){
-    	$("#step-1 ul li, #step-2 ul li, .hour-picker li").removeClass("active");
-    	$(this).addClass("active");
+    $(".choose-date").click(function (e) {
+        e.preventDefault();
+        $(".date-booking").removeClass('hidden');
     });
 
-     $('#datetimepicker12').datetimepicker({
-                inline: true,
-                sideBySide: false,
-                format:'d.m.Y',                
-                locale: 'fr',
-                viewMode: 'days',
-                daysOfWeekDisabled: [0, 6],
-                allowInputToggle: false
-            }).on('dp.change', function(){
-     			$(".hour-picker").removeClass('hidden');
-   			});;
+    $(".hour-picker, .list-select").on('click', 'li', function () {
+        $(".hour-picker li, .list-select li").removeClass("active");
+        $(this).addClass("active");
+    });
+    
+    $(".hour-picker").on('click', 'li', function () {
+        $(".hour-picker li").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('.datetimepicker').datetimepicker({
+        inline: true,
+        sideBySide: false,
+        format: 'd.m.Y',
+        locale: 'fr',
+        viewMode: 'days',
+        daysOfWeekDisabled: [0, 6],
+        allowInputToggle: false
+    }).on('dp.change', function () {
+        $(".hour-picker").removeClass('hidden');
+    });
 
 });
